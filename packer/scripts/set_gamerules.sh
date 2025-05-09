c8877f802752276fa3a9bf262350597adee53b88
VinceHPicton <54680328+VinceHPicton@users.noreply.github.com>
2025-05-09 16:06:40 +0100

    Update set_gamerules.sh

--- packer/scripts/set_gamerules.sh
+++ packer/scripts/set_gamerules.sh
@@ -5,3 +5,4 @@ source ./functions.sh
 cd /home/ubuntu
 send_to_screen bedrock "gamerule doInsomnia false"
 send_to_screen bedrock "gamerule mobGriefing false"
+send_to_screen bedrock "gamerule doTraderSpawning false"
