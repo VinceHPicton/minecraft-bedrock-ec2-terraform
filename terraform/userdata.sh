ebbe199ef30fff73f2bfb81ae7ceb36303105373
VinceHPicton <54680328+VinceHPicton@users.noreply.github.com>
2025-05-10 16:53:51 +0100

    userdata bug fix

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+cd /home/ubuntu
 aws s3 cp s3://mc-bedrock-backup/worlds.zip /home/ubuntu/worlds.zip
-unzip worlds.zip
-rm worlds.zip
+unzip ./worlds.zip
+rm ./worlds.zip
 aws s3 cp s3://mc-bedrock-backup/server.properties /home/ubuntu/
 aws s3 cp s3://mc-bedrock-backup/permissions.json /home/ubuntu/
 export LD_LIBRARY_PATH=.
